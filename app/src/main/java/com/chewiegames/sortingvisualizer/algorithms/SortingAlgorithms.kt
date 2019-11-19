9e5cb6675ea607bae49f509379be5bf8e13f7f84
David Alonso <david.alonsosantos1@gmail.com>
2019-11-20 00:06:56 +0100

    animations done :)

--- app/src/main/java/com/chewiegames/sortingvisualizer/algorithms/SortingAlgorithms.kt
+++ app/src/main/java/com/chewiegames/sortingvisualizer/algorithms/SortingAlgorithms.kt
@@ -1,65 +1,67 @@
 package com.chewiegames.sortingvisualizer.algorithms
 
-import android.util.Log
 import com.chewiegames.sortingvisualizer.Column
 import kotlin.math.floor
 
-const val NUMBER_OF_ARRAY_BARS = 50 //260 looking good
+const val NUMBER_OF_ARRAY_BARS = 310 //260 looking good
 const val SIZE_COLUMN = 380 //380 looking good
 const val ANIMATION_SPEED = 1L
 
-var animations = arrayListOf<Int>()
 
 private const val TAG = "SortingAlgorithms"
-fun getMergeSortAnimations(list: List<Column>) : List<Int>{
-    animations = arrayListOf()
-    doMergeSort(list)
+fun getMergeSortAnimations(list: List<Column>) : ArrayList<Array<Int>>{
+    val animations = arrayListOf<Array<Int>>()
+    doMergeSort(list, animations)
     return animations
 }
 
-fun doMergeSort(list: List<Column>): List<Column> {
+fun doMergeSort(list: List<Column>, animations: ArrayList<Array<Int>>): List<Column> {
     if(list.size <= 1) return list
     val middleIndex = floor((list.size / 2.0)).toInt()
     val left = list.subList(0, middleIndex)
     val right = list.subList(middleIndex, list.size)
-    return merge(doMergeSort(left), doMergeSort(right), middleIndex)
+    return merge(doMergeSort(left, animations), doMergeSort(right, animations), middleIndex, animations)
 }
 
-fun merge(left: List<Column>, right: List<Column>, middle: Int) : List<Column>{
+fun merge(left: List<Column>, right: List<Column>, middle: Int, animations: ArrayList<Array<Int>>) : List<Column>{
     val result = arrayListOf<Column>()
+    var k=0
     var leftIndex = 0
     var rightIndex = 0
     var middleIndex = middle
     while (leftIndex < left.count() && rightIndex < right.count()){
-        animations.add(middleIndex)
-        animations.add(middleIndex)
+        animations.add(arrayOf(leftIndex, middleIndex))
+        animations.add(arrayOf(leftIndex, middleIndex))
         if(left[leftIndex].value <= right[rightIndex].value){
-            animations.add(left[leftIndex].value)
+            animations.add(arrayOf(k, left[leftIndex].value))
             result.add(left[leftIndex])
             leftIndex++
         }else{
-            animations.add(right[rightIndex].value)
+            animations.add(arrayOf(k, right[rightIndex].value))
             result.add(right[rightIndex])
             rightIndex++
             middleIndex++
         }
+        k++
     }
 
     while (leftIndex < left.size){
-        animations.add(leftIndex)
-        animations.add(leftIndex)
-        animations.add(left[leftIndex].value)
+        animations.add(arrayOf(leftIndex, leftIndex))
+        animations.add(arrayOf(leftIndex, leftIndex))
+        animations.add(arrayOf(k, left[leftIndex].value))
         result.add(left[leftIndex])
         leftIndex++
+        k++
     }
 
     while(rightIndex < right.size){
-        animations.add(middleIndex)
-        animations.add(middleIndex)
-        animations.add(right[rightIndex].value)
+        animations.add(arrayOf(middleIndex, middleIndex))
+        animations.add(arrayOf(middleIndex, middleIndex))
+        animations.add(arrayOf(k, right[rightIndex].value))
         result.add(right[rightIndex])
         rightIndex++
         middleIndex++
+        k++
     }
     return result
 }
--- app/src/main/java/com/chewiegames/sortingvisualizer/ui/MainViewModel.kt
+++ app/src/main/java/com/chewiegames/sortingvisualizer/ui/MainViewModel.kt
@@ -3,9 +3,9 @@ package com.chewiegames.sortingvisualizer.ui
 import android.app.Activity
 import android.os.Handler
 import androidx.compose.Model
-import androidx.core.graphics.component1
-import androidx.core.graphics.component2
+import androidx.ui.core.Duration
 import androidx.ui.graphics.Color
+import androidx.ui.temputils.delay
 import com.chewiegames.sortingvisualizer.Column
 import com.chewiegames.sortingvisualizer.algorithms.*
 import kotlinx.coroutines.*
@@ -18,59 +18,68 @@ private const val TAG = "MainViewModel"
 object MainViewModel {
 
     var columns = arrayListOf<Column>()
-    private var color = Color.Red
+    private var animate = false
 
-    fun resetArray(): ArrayList<Column> {
-        val tempColumns = arrayListOf<Column>()
-        GlobalScope.launch(Dispatchers.Main) {
+    fun resetArray() {
+        columns = arrayListOf()
+        val  job = GlobalScope.launch(Dispatchers.Main) {
             for (i in 0 until NUMBER_OF_ARRAY_BARS) {
-                tempColumns.add(Column(i, randomNumberFromIntervals(5, SIZE_COLUMN), hashMapOf(), Color.Red))
-                columns = tempColumns
-                delay(ANIMATION_SPEED)
+                val column = Column(i, randomNumberFromIntervals(5, SIZE_COLUMN), hashMapOf(), Color.Red)
+                columns.add(column)
+                animate = true
+                delay(0)
             }
-            color = Color.Red
         }
-        return tempColumns
     }
 
     fun randomNumberFromIntervals(min: Int, max: Int) = floor(Math.random() * (max - min + 1) + min).toInt()
 
     fun mergeSort(array: List<Column>): List<Column> {
         //doMergeSort(array)
-        return doMergeSort(array)
-    }
-
-    private fun getMergeAnimations(array: List<Column>): List<Int> {
-        return getMergeSortAnimations(array)
+        return doMergeSort(array, arrayListOf())
     }
 
     fun onMergeSortSelected() {
+        val animations = getMergeSortAnimations(columns)
+
         GlobalScope.launch(Dispatchers.Main) {
-            val animations = getMergeSortAnimationsNew(columns)
             for (i in 0 until animations.size) {
                 val isColorChange = i % 3 != 2
                 if (isColorChange) {
-                    val columnOne = columns[animations[i] as Int]
-                    val columnTwo = columns[animations[i] as Int]
-                    val color = if (i % 3 == 0) Color.Cyan else Color.Magenta
-                    delay(ANIMATION_SPEED)
-                    columns[columnOne.id].color = color
-                    columns[columnTwo.id].color = color
+                    val (columnOneIndex, columnTwoIndex) = animations[i]
+                    val columnOne = columns[columnOneIndex]
+                    val columnTwo = columns[columnTwoIndex]
+                    val newColor = if (i % 3 == 0) Color.Cyan else Color.Magenta
+                    columns[columnOne.id].color = newColor
+                    columns[columnTwo.id].color = newColor
+                    animate = true
+                    delay(10)
                 } else {
-                    delay(ANIMATION_SPEED)
-                    val column = columns[animations[i] as Int]
-                    columns[column.id].value = animations[i] as Int
-
+                    val (columnOneIndex, newHeight) = animations[i]
+                    val column = columns[columnOneIndex]
+                    columns[column.id].value = newHeight
+                    animate = true
+                    delay(10)
                 }
             }
-            color = Color.Green
-//            columns = mergeSort(columns)
+            onComplete(Color.Green)
         }
+    }
+
+    private fun onComplete(finalColor: Color) {
+        GlobalScope.launch(Dispatchers.Main) {
+            for (i in columns.indices) {
+                columns[i].color = finalColor
+                animate = true
+                delay(0)
+            }
+        }
+
     }
 
     fun onColumnSelected(column: Column) {
         columns[column.id].color = Color.Yellow
-        color = Color.Yellow
+        animate = true
     }
 
     fun onNewSelected() = resetArray()
